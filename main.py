f4b031e3743c8728fb56aa62e4dbcd699ec40dd7
0xJohn <0xjohnw@gmail.com>
2024-06-07 02:58:02 +0700

    Add new version

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
+import os
 import time
 import random
-import os
-
+import math
 import cv2
 import keyboard
 import mss
@@ -10,17 +9,24 @@ import numpy as np
 import pygetwindow as gw
 import win32api
 import win32con
+import warnings
+from pywinauto import Application
 
-# Название окна с игрой - Узнать можно запустив скрипт python window_title.py
-WINDOW_TITLE = "Blum - SunBrowser"
-# Процент целей для обработки - настраивайте под себя, зависит от размера вашего монитора, окна с игрой, производительности ПК и тд. Больше значение - больше звезд собираем.
-TARGET_PERCENTAGE = 0.02
-# Путь к файлу шаблона кнопки Play - Сделайте свой скриншот кнопки, если не происходит автоматический запуск игр после их окончания.
-TEMPLATE_PATH = "template_play_button.png"
-# Порог совпадения шаблона - Можете попробовать уменьшить это значение, если не происходит автоматический запуск игр после их окончания.
-THRESHOLD = 0.8
 # Интервал проверки кнопки "Play" в секундах
 CHECK_INTERVAL = 5
+
+warnings.filterwarnings("ignore", category=UserWarning, module='pywinauto')
+
+
+def list_windows_by_title(title_keywords):
+    windows = gw.getAllWindows()
+    filtered_windows = []
+    for window in windows:
+        for keyword in title_keywords:
+            if keyword.lower() in window.title.lower():
+                filtered_windows.append((window.title, window._hWnd))
+                break
+    return filtered_windows
 
 
 class Logger:
@@ -35,20 +41,17 @@ class Logger:
 
 
 class AutoClicker:
-    def __init__(self, window_title, target_colors_hex, nearby_colors_hex, template_path, threshold, logger):
-        self.window_title = window_title
+    def __init__(self, hwnd, target_colors_hex, nearby_colors_hex, threshold, logger, target_percentage):
+        self.hwnd = hwnd
         self.target_colors_hex = target_colors_hex
         self.nearby_colors_hex = nearby_colors_hex
-        self.template_path = template_path
         self.threshold = threshold
         self.logger = logger
+        self.target_percentage = target_percentage
         self.running = False
         self.clicked_points = []
         self.iteration_count = 0
         self.last_check_time = time.time()
-
-        if not os.path.isfile(self.template_path):
-            raise FileNotFoundError(f"Файл шаблона не найден: {self.template_path}")
 
     @staticmethod
     def hex_to_hsv(hex_color):
@@ -92,43 +95,42 @@ class AutoClicker:
         current_time = time.time()
         if current_time - self.last_check_time >= CHECK_INTERVAL:
             self.last_check_time = current_time
-            template = cv2.imread(self.template_path, cv2.IMREAD_GRAYSCALE)
-            if template is None:
-                self.logger.log(f"Не удалось загрузить файл шаблона: {self.template_path}")
-                return
+            templates = [
+                cv2.imread(os.path.join("template_png", "template_play_button.png"), cv2.IMREAD_GRAYSCALE),
+                cv2.imread(os.path.join("template_png", "template_play_button1.png"), cv2.IMREAD_GRAYSCALE),
+                cv2.imread(os.path.join("template_png", "close_button.png"), cv2.IMREAD_GRAYSCALE)
+            ]
 
-            template_height, template_width = template.shape
+            for template in templates:
+                if template is None:
+                    self.logger.log("Не удалось загрузить файл шаблона.")
+                    continue
 
-            img = np.array(sct.grab(monitor))
-            img_gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
+                template_height, template_width = template.shape
 
-            res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-            loc = np.where(res >= self.threshold)
+                img = np.array(sct.grab(monitor))
+                img_gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
 
-            matched_points = list(zip(*loc[::-1]))
+                res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+                loc = np.where(res >= self.threshold)
 
-            if matched_points:
-                pt_x, pt_y = matched_points[0]
-                cX = pt_x + template_width // 2 + monitor["left"]
-                cY = pt_y + template_height // 2 + monitor["top"]
+                matched_points = list(zip(*loc[::-1]))
 
-                self.click_at(cX, cY)
-                self.logger.log(f'Нажал на кнопку "Play": {cX} {cY}')
-                self.clicked_points.append((cX, cY))
+                if matched_points:
+                    pt_x, pt_y = matched_points[0]
+                    cX = pt_x + template_width // 2 + monitor["left"]
+                    cY = pt_y + template_height // 2 + monitor["top"]
+
+                    self.click_at(cX, cY)
+                    self.logger.log(f'Нажал на кнопку "Play": {cX} {cY}')
+                    self.clicked_points.append((cX, cY))
+                    break  # Остановить проверку после первого найденного совпадения
 
     def click_color_areas(self):
-        windows = [win for win in gw.getAllTitles() if win == self.window_title]
-        if not windows:
-            self.logger.log(
-                f"Не найдено окна с заголовком: {self.window_title}. Откройте Веб-приложение Blum и откройте скрипт заново")
-            return
+        app = Application().connect(handle=self.hwnd)
+        window = app.window(handle=self.hwnd)
+        window.set_focus()
 
-        if len(windows) > 1:
-            self.logger.log(f"Обнаружено несколько окон с заголовком: {self.window_title}. Оставьте только одно окно с игрой.")
-            return
-
-        window = gw.getWindowsWithTitle(windows[0])[0]
-        window.activate()
         target_hsvs = [self.hex_to_hsv(color) for color in self.target_colors_hex]
         nearby_hsvs = [self.hex_to_hsv(color) for color in self.nearby_colors_hex]
 
@@ -137,11 +139,12 @@ class AutoClicker:
 
             while True:
                 if self.running:
+                    rect = window.rectangle()
                     monitor = {
-                        "top": window.top,
-                        "left": window.left,
-                        "width": window.width,
-                        "height": window.height
+                        "top": rect.top,
+                        "left": rect.left,
+                        "width": rect.width(),
+                        "height": rect.height()
                     }
                     img = np.array(sct.grab(monitor))
                     img_bgr = cv2.cvtColor(img, cv2.COLOR_BGRA2BGR)
@@ -154,11 +157,11 @@ class AutoClicker:
                         contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
                         num_contours = len(contours)
-                        num_to_click = int(num_contours * TARGET_PERCENTAGE)
+                        num_to_click = int(num_contours * self.target_percentage)
                         contours_to_click = random.sample(contours, num_to_click)
 
                         for contour in reversed(contours_to_click):
-                            if cv2.contourArea(contour) < 1:
+                            if cv2.contourArea(contour) < 6:
                                 continue
 
                             M = cv2.moments(contour)
@@ -186,27 +189,51 @@ class AutoClicker:
 
 
 if __name__ == "__main__":
-    # Убедитесь, что текущая рабочая директория соответствует директории скрипта
     current_dir = os.path.dirname(os.path.abspath(__file__))
     os.chdir(current_dir)
+
+    keywords = ["Blum", "Telegram"]
+    windows = list_windows_by_title(keywords)
+
+    if not windows:
+        print("Нет окон, содержащих указанные ключевые слова Blum или Telegram.")
+        exit()
+
+    print("Доступные окна для выбора:")
+    for i, (title, hwnd) in enumerate(windows):
+        print(f"{i + 1}: {title}")
+
+    choice = int(input("Введите номер окна, в котором открыт бот Blum: ")) - 1
+    if choice < 0 or choice >= len(windows):
+        print("Неверный выбор.")
+        exit()
+
+    hwnd = windows[choice][1]
+
+    while True:
+        try:
+            target_percentage = input("Введите значение от 0 до 1 для рандомизации прокликивания звезд, где 1 означает сбор всех звезд. (Выбор величины зависит от множества факторов: размера экрана, окна и т.д.) Я выбираю значения 0.04 - 0.06 для сбора около 140-150 звезд. Вам необходимо самостоятельно подобрать необходимое значение: ")
+            target_percentage = target_percentage.replace(',', '.')
+            target_percentage = float(target_percentage)
+            if 0 <= target_percentage <= 1:
+                break
+            else:
+                print("Пожалуйста, введите значение от 0 до 1.")
+        except ValueError:
+            print("Неверный формат. Пожалуйста, введите число.")
 
     logger = Logger("[https://t.me/x_0xJohn]")
     logger.log("Вас приветствует бесплатный скрипт - автокликер для игры Blum")
     logger.log('После запуска мини игры нажимайте клавишу F6 на клавиатуре')
     target_colors_hex = ["#c9e100", "#bae70e"]
     nearby_colors_hex = ["#abff61", "#87ff27"]
-    template_path = os.path.join(current_dir, TEMPLATE_PATH)
-    threshold = THRESHOLD  # Порог совпадения шаблона
+    threshold = 0.8  # Порог совпадения шаблона
 
-    if not os.path.isfile(template_path):
-        raise FileNotFoundError(f"Файл шаблона не найден: {template_path}")
-
-    auto_clicker = AutoClicker(WINDOW_TITLE, target_colors_hex, nearby_colors_hex, template_path, threshold, logger)
+    auto_clicker = AutoClicker(hwnd, target_colors_hex, nearby_colors_hex, threshold, logger, target_percentage)
     try:
         auto_clicker.click_color_areas()
     except Exception as e:
         logger.log(f"Произошла ошибка: {e}")
     for i in reversed(range(5)):
-        i += 1
         print(f"Скрипт завершит работу через {i}")
         time.sleep(1)
