e5649307fb4daaaea346e88260e7aa6a60dbf736
0xJohn <0xjohnw@gmail.com>
2024-06-06 16:15:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
 import time
 import random
+import os
 
 import cv2
 import keyboard
@@ -10,12 +11,17 @@ import pygetwindow as gw
 import win32api
 import win32con
 
-# Название окна с игрой - Замените на свое, если возникает ошибка "Не найдено окна с заголовком:"
-WINDOW_TITLE = "Blum"
-# Процент целей для обработки (0.018 - примерно 130 звездочек за игру)
-TARGET_PERCENTAGE = 0.018
+# Название окна с игрой - Узнать можно запустив скрипт python window_title.py
+WINDOW_TITLE = "Blum - SunBrowser"
+# Процент целей для обработки - настраивайте под себя, зависит от размера вашего монитора, окна с игрой, производительности ПК и тд. Больше значение - больше звезд собираем.
+TARGET_PERCENTAGE = 0.02
+# Путь к файлу шаблона кнопки Play - Сделайте свой скриншот кнопки, если не происходит автоматический запуск игр после их окончания.
+TEMPLATE_PATH = "template_play_button.png"
+# Порог совпадения шаблона - Можете попробовать уменьшить это значение, если не происходит автоматический запуск игр после их окончания.
+THRESHOLD = 0.8
 # Интервал проверки кнопки "Play" в секундах
-CHECK_INTERVAL = 5  
+CHECK_INTERVAL = 5
+
 
 class Logger:
     def __init__(self, prefix=None):
@@ -41,6 +47,9 @@ class AutoClicker:
         self.iteration_count = 0
         self.last_check_time = time.time()
 
+        if not os.path.isfile(self.template_path):
+            raise FileNotFoundError(f"Файл шаблона не найден: {self.template_path}")
+
     @staticmethod
     def hex_to_hsv(hex_color):
         hex_color = hex_color.lstrip('#')
@@ -52,9 +61,14 @@ class AutoClicker:
 
     @staticmethod
     def click_at(x, y):
-        win32api.SetCursorPos((x, y))
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
-        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+        try:
+            if not (0 <= x < win32api.GetSystemMetrics(0) and 0 <= y < win32api.GetSystemMetrics(1)):
+                raise ValueError(f"Координаты вне пределов экрана: ({x}, {y})")
+            win32api.SetCursorPos((x, y))
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+        except Exception as e:
+            print(f"Ошибка при установке позиции курсора: {e}")
 
     def toggle_script(self):
         self.running = not self.running
@@ -79,6 +93,10 @@ class AutoClicker:
         if current_time - self.last_check_time >= CHECK_INTERVAL:
             self.last_check_time = current_time
             template = cv2.imread(self.template_path, cv2.IMREAD_GRAYSCALE)
+            if template is None:
+                self.logger.log(f"Не удалось загрузить файл шаблона: {self.template_path}")
+                return
+
             template_height, template_width = template.shape
 
             img = np.array(sct.grab(monitor))
@@ -99,13 +117,17 @@ class AutoClicker:
                 self.clicked_points.append((cX, cY))
 
     def click_color_areas(self):
-        windows = gw.getWindowsWithTitle(self.window_title)
+        windows = [win for win in gw.getAllTitles() if win == self.window_title]
         if not windows:
             self.logger.log(
                 f"Не найдено окна с заголовком: {self.window_title}. Откройте Веб-приложение Blum и откройте скрипт заново")
             return
 
-        window = windows[0]
+        if len(windows) > 1:
+            self.logger.log(f"Обнаружено несколько окон с заголовком: {self.window_title}. Оставьте только одно окно с игрой.")
+            return
+
+        window = gw.getWindowsWithTitle(windows[0])[0]
         window.activate()
         target_hsvs = [self.hex_to_hsv(color) for color in self.target_colors_hex]
         nearby_hsvs = [self.hex_to_hsv(color) for color in self.nearby_colors_hex]
@@ -164,13 +186,21 @@ class AutoClicker:
 
 
 if __name__ == "__main__":
+    # Убедитесь, что текущая рабочая директория соответствует директории скрипта
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(current_dir)
+
     logger = Logger("[https://t.me/x_0xJohn]")
     logger.log("Вас приветствует бесплатный скрипт - автокликер для игры Blum")
     logger.log('После запуска мини игры нажимайте клавишу F6 на клавиатуре')
     target_colors_hex = ["#c9e100", "#bae70e"]
     nearby_colors_hex = ["#abff61", "#87ff27"]
-    template_path = "template_play_button.png"
-    threshold = 0.8  # Порог совпадения шаблона
+    template_path = os.path.join(current_dir, TEMPLATE_PATH)
+    threshold = THRESHOLD  # Порог совпадения шаблона
+
+    if not os.path.isfile(template_path):
+        raise FileNotFoundError(f"Файл шаблона не найден: {template_path}")
+
     auto_clicker = AutoClicker(WINDOW_TITLE, target_colors_hex, nearby_colors_hex, template_path, threshold, logger)
     try:
         auto_clicker.click_color_areas()
